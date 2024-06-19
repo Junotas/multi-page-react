653d5627f95f319e0d5b5ea113e6d045cdf16143
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-06-19 07:56:33 +0200

    implemented basic layout page now has three pages home about and contacts

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const about: React.FC = () => {
+  return (
+    <div className='container'>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+export default about;
---
+++ src/pages/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const contact: React.FC = () => {
+  return (
+    <div className='container'>
+      <h1>Contact Page</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+};
+
+export default contact;
